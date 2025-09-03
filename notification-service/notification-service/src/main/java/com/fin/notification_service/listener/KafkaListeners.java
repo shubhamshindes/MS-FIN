ecddea30eaae60f81983c36a54b55af5cfc62306
shubhamshindes <shubhamshindes.dev@gmail.com>
2025-09-03 11:12:12 +0530

    mail update

--- notification-service/notification-service/src/main/java/com/fin/notification_service/listener/KafkaListeners.java
+++ notification-service/notification-service/src/main/java/com/fin/notification_service/listener/KafkaListeners.java
@@ -20,27 +20,27 @@ public class KafkaListeners {
     @KafkaListener(topics = "customer-deleted", groupId = "notification-group")
     public void listenCustomerDeleted(String message) {
         System.out.println("Received message: " + message);
-        sendEmail("admin@loanapp.com", "Customer Deleted", message);
+        sendEmail("shindeshubham8055@gmail.com", "Customer Deleted", message);
     }
 
     @KafkaListener(topics = "disbursement-created", groupId = "notification-group")
     public void listenDisbursementCreated(String message) {
         System.out.println("Received message: " + message);
-        sendEmail("admin@loanapp.com", "New Disbursement Created", message);
+        sendEmail("shindeshubham8055@gmail.com", "New Disbursement Created", message);
     }
 
     @KafkaListener(topics = "payment-reminders", groupId = "notification-group")
     public void listenPaymentReminders(String message) {
         System.out.println("Received payment reminder: " + message);
         // In a real application, you would send this to the specific customer
-        sendEmail("customers@loanapp.com", "Payment Reminder", message);
+        sendEmail("shindeshubham8055@gmail.com", "Payment Reminder", message);
     }
 
     @KafkaListener(topics = "overdue-payments", groupId = "notification-group")
     public void listenOverduePayments(String message) {
         System.out.println("Received overdue payment notification: " + message);
         // In a real application, you would send this to the specific customer
-        sendEmail("customers@loanapp.com", "Overdue Payment Notification", message);
+        sendEmail("shindeshubham8055@gmail.com", "Overdue Payment Notification", message);
     }
 
     private void sendEmail(String to, String subject, String text) {
